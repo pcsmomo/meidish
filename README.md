f020dffda553311dceb9d1e5f264b88c38440cce
pcsmomo <dongwoo_333@hanmail.net>
2021-09-05 20:15:11 +1000

    config/webpack: add css/sass loader

--- README.md
+++ README.md
@@ -6,7 +6,12 @@ Custom template set webpack, babel, eslint and so on.
 
 1. Set up the front end part and build with webpack
    1. install react
-      `npm install --save react react-router-dom`
+      `npm install --save react react-dom react-router-dom`
    2. webpack config
       1. install
          `npm install --save-dev webpack webpack-cli`
+   3. Loaders
+      1. images
+      2. txt
+      3. css/sass - loaders
+         `npm install --save-dev style-loader css-loader sass-loader sass`
--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,24 @@ module.exports = {
     path: path.resolve(__dirname, "./build"),
   },
   mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|jpeg)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.txt/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
 };
