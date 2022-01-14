598060bedb19bca6a9fa5dbbb05e71c605bfd660
pcsmomo <dongwoo_333@hanmail.net>
2022-01-15 07:58:41 +1100

    style: complete sign page layout

--- src/components/Signin/index.tsx
+++ src/components/Signin/index.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Signin = () => {
-  return <div>Sign in</div>;
+  return (
+    <div className="row" style={{ marginTop: 24 }}>
+      <form className="col s12 m6 offset-m3">
+        <div className="row">
+          <div className="input-field col s12">
+            <input name="username" type="text" />
+            <label htmlFor="username">Username</label>
+          </div>
+          <div className="input-field col s12">
+            <input name="password" type="password" />
+            <label htmlFor="password">Password</label>
+          </div>
+          <button className="btn col s12">
+            <span>Login</span>
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Signin;
