34de7010dfdf9eb7080d873e4289e5f1c4fab8ea
pcsmomo <dongwoo_333@hanmail.net>
2022-01-16 14:34:54 +1100

    feat: create custom hooks, useActions and useTypedSelector

--- src/components/Signin/index.tsx
+++ src/components/Signin/index.tsx
@@ -1,12 +1,31 @@
-import React from "react";
+import { FormEvent, FormEventHandler, useEffect } from "react";
+import { useActions } from "../../hooks/useActions";
+import { useTypedSelector } from "../../hooks/useTypedSelector";
+import M from "materialize-css";
 
 const Signin = () => {
+  const { fetchUser } = useActions();
+
+  const { loading, user } = useTypedSelector((state) => state.user);
+
+  useEffect(() => {
+    fetchUser(2);
+  }, []);
+
+  useEffect(() => {
+    M.updateTextFields();
+  }, [user]);
+
+  const handleSubmit: FormEventHandler = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="row" style={{ marginTop: 24 }}>
-      <form className="col s12 m6 offset-m3">
+      <form onSubmit={handleSubmit} className="col s12 m6 offset-m3">
         <div className="row">
           <div className="input-field col s12">
-            <input name="username" type="text" />
+            <input name="username" type="text" value={user?.username} />
             <label htmlFor="username">Username</label>
           </div>
           <div className="input-field col s12">
